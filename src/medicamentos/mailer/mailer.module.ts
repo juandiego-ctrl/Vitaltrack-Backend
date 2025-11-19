2a480f277051acae31f198a8817ee3cc9b145894
juandiego-ctrl <juandiego.cdp@gmail.com>
2025-11-19 15:24:58 -0500

    correcciones

--- src/medicamentos/mailer/mailer.module.ts
+++ src/medicamentos/mailer/mailer.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common';
+// src/medicamentos/mailer/mailer.module.ts
+import { Global, Module } from '@nestjs/common';  // ← Agrega "Global" aquí
 import { MailerService } from './mailer.service';
 
+@Global()   // ← ESTA LÍNEA ES LA QUE RESUELVE TODO
 @Module({
   providers: [MailerService],
   exports: [MailerService],
