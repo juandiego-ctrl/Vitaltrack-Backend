2850fdfa433a4e5810712fc8ac7b2d244a0deb17
juandiego-ctrl <juandiego.cdp@gmail.com>
2025-12-07 13:22:27 -0500

    correcciones

--- src/excelarchivo/excelarchivo.module.ts
+++ src/excelarchivo/excelarchivo.module.ts
@@ -4,6 +4,7 @@ import { ExcelarchivoController } from './excelarchivo.controller';
 import { ExcelarchivoService } from './excelarchivo.service';
 import { ExcelarchivoSchema } from './excelarchivo.modelo';
 
+// Importar módulos
 import { PacienteModule } from '../paciente/paciente.module';
 import { diagnosticoModule } from '../diagnostico/diagnostico.module';
 import { AntecedentesModule } from '../antecedentes/antecedentes.module';
@@ -15,10 +16,41 @@ import { ttoqtModule } from '../ttoqt/ttoqt.module';
 import { ttortModule } from '../ttort/ttort.module';
 import { ttotrasplanteModule } from '../ttotrasplante/ttotrasplante.module';
 import { UsuarioModule } from '../usuario/usuario.module';
+import { CitasModule } from '../citas/citas.module';
+import { MedicamentosModule } from '../medicamentos/medicamentos.module';
+
+// Importar esquemas para inyección directa
+import { diagnosticoSchema } from '../diagnostico/diagnostico.modelo';
+import { AntecedentesSchema } from '../antecedentes/antecedentes.modelo';
+import { ttocxSchema } from '../ttocx/ttocx.modelo';
+import { ttortSchema } from '../ttort/ttort.modelo';
+import { ttoqtSchema } from '../ttoqt/ttoqt.modelo';
+import { ttocxreconstructivaSchema } from '../ttocxreconstructiva/ttocxreconstructiva.modelo';
+import { ttopaliativosSchema } from '../ttopaliativos/ttopaliativos.modelo';
+import { ttotrasplanteSchema } from '../ttotrasplante/ttotrasplante.modelo';
+import { CitaSchema } from '../citas/schemas/cita.schema';
+import { MedicamentoSchema } from '../medicamentos/schema/medicamento.schema';
+import { archivospacientesSchema } from '../archivospacientes/archivospacientes.modelo';
+import { ZipSoportesSchema } from '../zipsoportes/zipsoportes.modelo';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Excelarchivo', schema: ExcelarchivoSchema }]), // ✅ Cambiar a 'Excelarchivo'
+    MongooseModule.forFeature([
+      { name: 'Excelarchivo', schema: ExcelarchivoSchema },
+      // Modelos para consulta unificada
+      { name: 'diagnostico', schema: diagnosticoSchema },
+      { name: 'antecedentes', schema: AntecedentesSchema },
+      { name: 'ttoqt', schema: ttoqtSchema },
+      { name: 'ttort', schema: ttortSchema },
+      { name: 'ttocx', schema: ttocxSchema },
+      { name: 'ttocxreconstructiva', schema: ttocxreconstructivaSchema },
+      { name: 'ttopaliativos', schema: ttopaliativosSchema },
+      { name: 'ttotrasplante', schema: ttotrasplanteSchema },
+      { name: 'Cita', schema: CitaSchema },
+      { name: 'Medicamento', schema: MedicamentoSchema },
+      { name: 'archivospacientes', schema: archivospacientesSchema },
+      { name: 'zipsoportes', schema: ZipSoportesSchema },
+    ]),
     PacienteModule,
     diagnosticoModule,
     AntecedentesModule,
@@ -30,6 +62,8 @@ import { UsuarioModule } from '../usuario/usuario.module';
     ttortModule,
     ttotrasplanteModule,
     UsuarioModule,
+    CitasModule,
+    MedicamentosModule,
   ],
   controllers: [ExcelarchivoController],
   providers: [ExcelarchivoService],
